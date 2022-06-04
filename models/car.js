fe6fa2878b9191977c8dacd62e88bfb754c3c99e
Logan Monson <scarletfedora@gmail.com>
2022-06-04 14:22:36 -0700

    accepted incoming model changes from develop branch

---
+++ models/car.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Car extends Model {}
+
+Car.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    vin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    miles: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fuel_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    transmission: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    engine: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    starting_bid: {
+      type: DataTypes.FLOAT,
+    },
+    bid_increment: {
+      type: DataTypes.FLOAT,
+    },
+    current_bid: {
+      type: DataTypes.FLOAT,
+    },
+    seller_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'car',
+  }
+);
+
+module.exports = Car;
