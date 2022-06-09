4dd438b0b7743481373681dd5a72335e48ea3032
Logan Monson <scarletfedora@gmail.com>
2022-06-08 23:51:42 -0700

    halfway thru bidmaking

--- public/js/new-bid.js
+++ public/js/new-bid.js
@@ -1,18 +1,29 @@
 const createBid = async (event) => {
   event.preventDefault();
 
-  const newBid = document.querySelector('#newBid').value.trim();
-  const currBid = document.querySelector('#currBid').value.trim();
-  const bidInc = document.querySelector('#bidInc').value.trim();
+  const car_id = document.querySelector('input[name="car-id"]').value;
+
+  const bid = document.querySelector('#newBid').value.trim();
+  const currBid = document.querySelector('#currBid').value;
+  const bidInc = document.querySelector('#bidInc').value;
 
   const reqBid = currBid + bidInc;
 
-  if (newBid >= reqBid) {
+  console.log(bid);
+  console.log(car_id);
+  console.log(typeof currBid);
+  console.log(typeof bidInc);
+  console.log(reqBid);
+
+  if (bid >= reqBid) {
     const response = await fetch('/api/bids', {
       method: 'POST',
-      body: JSON.stringify({ newBid }),
+      body: JSON.stringify({ bid, car_id }),
       headers: { 'Content-Type': 'application/json' },
     });
+
+    console.log(bid);
+    console.log(car_id);
 
     if (response.ok) {
       document.location.replace('/');
